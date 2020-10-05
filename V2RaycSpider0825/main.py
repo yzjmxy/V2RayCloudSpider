287021a77ed252390dbcf0ba15cc0dd6fe751bb3
QIN2DIM <RmAlkaid@outlook.com>
2020-10-06 01:51:05 +0800

    V2Ray云彩姬-v4.2.0

--- V2RaycSpider0825/main.py
+++
@@ -1,9 +0,0 @@
-from Panel.main_panel import *
-
-"""欢迎使用V2Ray云彩姬"""
-if __name__ == '__main__':
-    hp = HomePanel()
-    try:
-        hp.start(init=True)
-    except Exception as e:
-        hp.kill(e)
